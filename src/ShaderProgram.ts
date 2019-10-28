4109e00ea82fd051c3ebf908ee2704382e42146b
gunax <gunax@gunax.com>
2019-10-28 13:24:04 -0500

    add shaderprogram class

---
+++ src/ShaderProgram.ts
@@ -0,0 +1,56 @@
+class ShaderProgram {
+
+    private gl : WebGLRenderingContext;
+    private vShader : WebGLShader;
+    private fShader : WebGLShader;
+    private program : WebGLProgram;
+
+    constructor(gl : WebGLRenderingContext, vsource : string, fsource : string) {
+        this.gl = gl;
+        this.program = gl.createProgram();
+
+        this.vShader = this.loadShader(this.gl.VERTEX_SHADER, vsource);
+        this.fShader = this.loadShader(this.gl.FRAGMENT_SHADER, fsource);
+
+        this.gl.attachShader(this.program, this.vShader);
+        this.gl.attachShader(this.program, this.fShader);
+        this.gl.linkProgram(this.program);
+    }
+
+    loadShader(type: number, source: string) {
+        const shader = this.gl.createShader(type);
+        this.gl.shaderSource(shader, source);
+        this.gl.compileShader(shader);
+
+        if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
+            console.log('Could not compile shader ' + this.gl.getShaderInfoLog(shader));
+            this.gl.deleteShader(shader);
+        }
+        return shader;
+    }
+
+    getProgram() {
+        return this.program;
+    }
+
+    getProgramInfo() {
+        return {
+            program: this.program,
+            attribLocations: {
+                vertexPosition: this.gl.getAttribLocation(this.program, 'aVertexPosition'),
+            },
+            uniformLocations: {
+                projectionMatrix: this.gl.getUniformLocation(this.program, 'uProjectionMatrix'),
+                modelViewMatrix: this.gl.getUniformLocation(this.program, 'uModelViewMatrix'),
+            },
+        };
+    }
+}
+
+interface ProgramInfo {
+    program : WebGLProgram;
+    attribLocations? : any;
+    uniformLocations? : any;
+}
+
+export {ShaderProgram, ProgramInfo};
--- src/opengl_canvas.ts
+++ src/opengl_canvas.ts
@@ -1,69 +1,27 @@
 import { vsSource, fsSource } from "./Shaders";
+import {ShaderProgram, ProgramInfo} from "./ShaderProgram";
 import { mat4 } from "./gl-matrix";
 import { Model } from "./Model";
 
 export class OpenGLCanvas {
     canvas: HTMLCanvasElement;
     gl: WebGLRenderingContext;
-    shaderProgram: WebGLProgram;
-    programInfo: any;
-    buffers: any;
+    shaderProgram : ShaderProgram;
     my_model : Model;
+
     constructor() {
         this.canvas = <HTMLCanvasElement>document.getElementById('glCanvas');
         this.gl = this.canvas.getContext('webgl');
         this.prepare();
-        this.programInfo = this.initShaders();
-        this.buffers = this.initBuffer();
-        this.render(this.gl, this.programInfo, this.buffers);
+        this.shaderProgram = new ShaderProgram(this.gl, vsSource, fsSource);
+        this.my_model = this.initBuffer();
+        this.render(this.gl, this.shaderProgram.getProgramInfo(), this.my_model);
     }
 
     prepare() {
         if (this.gl === null) {
             document.body.textContent = 'WebGL is not avaiulable';
         }
-
-        // Set clear color to black, fully opaque
-        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        // Clear the color buffer with specified clear color
-        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
-    }
-
-    initShaders() {
-        const vShader = this.loadShader(this.gl.VERTEX_SHADER, vsSource);
-        const fShader = this.loadShader(this.gl.FRAGMENT_SHADER, fsSource);
-
-        this.shaderProgram = this.gl.createProgram();
-        this.gl.attachShader(this.shaderProgram, vShader);
-        this.gl.attachShader(this.shaderProgram, fShader);
-        this.gl.linkProgram(this.shaderProgram);
-
-        const programInfo = {
-            program: this.shaderProgram,
-            attribLocations: {
-                vertexPosition: this.gl.getAttribLocation(this.shaderProgram, 'aVertexPosition'),
-            },
-            uniformLocations: {
-                projectionMatrix: this.gl.getUniformLocation(this.shaderProgram, 'uProjectionMatrix'),
-                modelViewMatrix: this.gl.getUniformLocation(this.shaderProgram, 'uModelViewMatrix'),
-            },
-        };
-
-        console.log(programInfo.attribLocations.vertexPosition);
-
-        return programInfo;
-    }
-
-    loadShader(type: number, source: string) {
-        const shader = this.gl.createShader(type);
-        this.gl.shaderSource(shader, source);
-        this.gl.compileShader(shader);
-
-        if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
-            console.log('Could not compile shader ' + this.gl.getShaderInfoLog(shader));
-            this.gl.deleteShader(shader);
-        }
-        return shader;
     }
 
     initBuffer() {
@@ -115,17 +73,13 @@ export class OpenGLCanvas {
             20, 21, 22, 20, 22, 23,   // left
         ];
 
-        this.my_model = new Model(new Float32Array(positions), new Uint16Array(indices));
-        this.my_model.load(this.gl);
+        let model = new Model(new Float32Array(positions), new Uint16Array(indices));
+        model.load(this.gl);
 
-        return {
-            position: this.my_model.Buffers.vertices,
-            //color: colorBuffer,
-            indices: this.my_model.Buffers.vertices
-        };
+        return model;
     }
 
-    render(gl: WebGLRenderingContext, programInfo: any, buffers: any) {
+    render(gl: WebGLRenderingContext, programInfo: ProgramInfo, model : Model) {
         gl.clearColor(0.3, 0.1, 0.1, 1.0);  // Clear to black, fully opaque
         gl.clearDepth(1.0);                 // Clear everything
         gl.enable(gl.DEPTH_TEST);           // Enable depth testing
@@ -165,7 +119,7 @@ export class OpenGLCanvas {
 
         mat4.translate(modelViewMatrix,     // destination matrix
             modelViewMatrix,     // matrix to translate
-            [-0.0, 0.0, -6.0]);  // amount to translate
+            [-0.0, 0.0, -8.0]);  // amount to translate
 
         // Tell WebGL how to pull out the positions from the position
         // buffer into the vertexPosition attribute.
@@ -176,7 +130,7 @@ export class OpenGLCanvas {
             const stride = 0;         // how many bytes to get from one set of values to the next
             // 0 = use type and numComponents above
             const offset = 0;         // how many bytes inside the buffer to start from
-            gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+            gl.bindBuffer(gl.ARRAY_BUFFER, model.Buffers.vertices);
             gl.vertexAttribPointer(
                 programInfo.attribLocations.vertexPosition,
                 numComponents,
