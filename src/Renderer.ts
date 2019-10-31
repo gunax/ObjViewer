65cb391ebcc66cc5706545a6395bbb8caedd65a6
gunax <gunax@gunax.com>
2019-10-30 23:46:29 -0500

    add simple mouse wheel zooming

---
+++ src/Renderer.ts
@@ -0,0 +1,5 @@
+import { Camera } from "./Camera";
+
+class Renderer {
+    constructor(private camera : Camera, private gl : WebGLRenderingContext) {}
+}
--- src/opengl_canvas.ts
+++ src/opengl_canvas.ts
@@ -10,14 +10,24 @@ export class OpenGLCanvas {
     gl: WebGLRenderingContext;
     shaderProgram : ShaderProgram;
     my_model : Model;
+    camera : Camera;
 
     constructor(canvas : HTMLElement) {
         this.canvas = <HTMLCanvasElement>canvas;
+        this.camera = Camera.getDefaultCamera();
+        this.camera.translate(0.1, 0.5, 0.0); //move the camera off-center a bit for better viewing
+        this.canvas.addEventListener("wheel", this.zoom);
         this.gl = this.canvas.getContext('webgl');
         this.prepare();
         this.shaderProgram = new ShaderProgram(this.gl, vsSource, fsSource);
         this.my_model = this.initModel();
-        this.render(this.gl, this.my_model);
+        this.render();
+    }
+
+    public zoom = (event : Event) => {
+        let wheelEvent = <WheelEvent> event;
+        this.camera.translate(0.0, 0.0, wheelEvent.deltaY*0.2);
+        this.render();
     }
 
     prepare() {
@@ -33,58 +43,55 @@ export class OpenGLCanvas {
         return model;
     }
 
-    render(gl: WebGLRenderingContext, model : Model) {
-        gl.clearColor(0.3, 0.1, 0.1, 1.0);  // Clear to black, fully opaque
-        gl.clearDepth(1.0);                 // Clear everything
-        gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-        gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+    render() {
+        this.gl.clearColor(0.3, 0.1, 0.1, 1.0);  // Clear to black, fully opaque
+        this.gl.clearDepth(1.0);                 // Clear everything
+        this.gl.enable(this.gl.DEPTH_TEST);           // Enable depth testing
+        this.gl.depthFunc(this.gl.LEQUAL);            // Near things obscure far things
 
         // Clear the canvas before we start drawing on it.
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-        let camera = Camera.getDefaultCamera();
-        camera.translate(0.1, 0.5, 0.0); //move the camera off-center a bit for better viewing
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
 
         // Tell WebGL how to pull out the positions from the position
         // buffer into the vertexPosition attribute.
     {
             const numComponents = 3;  // pull out 2 values per iteration
-            const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+            const type = this.gl.FLOAT;    // the data in the buffer is 32bit floats
             const normalize = false;  // don't normalize
             const stride = 0;         // how many bytes to get from one set of values to the next
             // 0 = use type and numComponents above
             const offset = 0;         // how many bytes inside the buffer to start from
-            gl.bindBuffer(gl.ARRAY_BUFFER, model.Buffers.vertices);
-            gl.vertexAttribPointer(
+            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.my_model.Buffers.vertices);
+            this.gl.vertexAttribPointer(
                 this.shaderProgram.getProgramInfo().attribLocations.vertexPosition,
                 numComponents,
                 type,
                 normalize,
                 stride,
                 offset);
-            gl.enableVertexAttribArray(
+            this.gl.enableVertexAttribArray(
                 this.shaderProgram.getProgramInfo().attribLocations.vertexPosition);
         }
 
         // Tell WebGL to use our program when drawing
-        gl.useProgram(this.shaderProgram.getProgram());
+        this.gl.useProgram(this.shaderProgram.getProgram());
 
         // Set the shader uniforms
 
-        gl.uniformMatrix4fv(
+        this.gl.uniformMatrix4fv(
             this.shaderProgram.getProgramInfo().uniformLocations.projectionMatrix,
             false,
-            camera.projection);
-        gl.uniformMatrix4fv(
+            this.camera.projection);
+        this.gl.uniformMatrix4fv(
             this.shaderProgram.getProgramInfo().uniformLocations.modelViewMatrix,
             false,
-            camera.modelView);
+            this.camera.modelView);
 
         {
             const offset = 0;
             const vertexCount = 36;
-            const type = gl.UNSIGNED_SHORT;
-            gl.drawElements(gl.LINE_STRIP, vertexCount, type, offset);
+            const type = this.gl.UNSIGNED_SHORT;
+            this.gl.drawElements(this.gl.LINE_STRIP, vertexCount, type, offset);
         }
     }
 }  
